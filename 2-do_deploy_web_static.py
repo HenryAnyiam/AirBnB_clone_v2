c94200a9fa648c7c837bf97bdc0b5532ce6e07c0
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-11 00:28:15 -0700

    fixed deploy error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,20 +15,19 @@ def do_deploy(archive_path):
     Args:
         archive_path: path to file
     """
-    env.hosts = ["ubuntu@3.86.7.132", "ubuntu@54.167.172.117"]
     if os.path.exists(archive_path):
         try:
             new_file = archive_path.split('/')[-1]
             new_path = "/tmp/" + new_file
             new_folder = "/data/web_static/releases/" + new_file.split('.')[0]
             put(archive_path, new_path)
-            run(f"sudo mkdir -p {new_folder}")
-            run(f"sudo tar -xzf {new_path} -C {new_folder}")
-            run(f"sudo rm {new_path}")
-            run(f"sudo mv -n {new_folder}/web_static/* {new_folder}")
-            run(f"sudo rm -rf {new_folder}/web_static/")
-            run("sudo rm -rf /data/web_static/current")
-            run(f"sudo ln -s {new_folder} /data/web_static/current")
+            run(f"mkdir -p {new_folder}")
+            run(f"tar -xzf {new_path} -C {new_folder}")
+            run(f"rm {new_path}")
+            run(f"mv -n {new_folder}/web_static/* {new_folder}")
+            run(f"rm -rf {new_folder}/web_static/")
+            run("rm -rf /data/web_static/current")
+            run(f"ln -s {new_folder}/ /data/web_static/current")
             print("New version deployed!")
             return True
         except Exception:
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,9 +3,9 @@
 
 from fabric.api import *
 import os
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
 from datetime import datetime
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 env.hosts = ["ubuntu@3.86.7.132", "ubuntu@54.167.172.117"]
@@ -13,5 +13,8 @@ env.key_filename = "~/.ssh/school"
 
 
 def deploy():
-    output_file = do_pack
-    if output_file != None;
+    output_file = do_pack()
+    if output_file is not None:
+        return do_deploy(output_file)
+    else:
+        return False
