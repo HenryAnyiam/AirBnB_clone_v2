a08c310cb2541fd5309be469b66d5b4d6c0986eb
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-24 12:07:39 -0700

    web flssk

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """building AirBnB web framework"""
 from flask import Flask, render_template
-from models import storage
+from models import storafe
+from models import *
 
 
 app = Flask(__name__)
@@ -9,7 +10,17 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def list_states():
+    """list out availablr states"""
+    states = list(storage.all(States).values())
+    states.sort(key=lambda x: x.name)
     return render_template("7-states_list.html")
+
+
+@app.teardown_appcontext
+def tear_down(exception=None):
+    """close session"""
+    storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
